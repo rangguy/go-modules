2d36c67bc192a1e0ed792834ebb5af1bd70c2d9e
rangguy <ranggadwi100@gmail.com>
2024-10-21 21:22:45 +0700

    go modules

---
+++ app-hello/go.mod
@@ -0,0 +1,5 @@
+module github.com/rangguy/app-hello.git
+
+go 1.23.0
+
+require github.com/rangguy/go-hello/v2 v2.0.0
---
+++ app-hello/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	say_hello "github.com/rangguy/go-hello/v2"
+)
+
+func main() {
+	fmt.Println(say_hello.SayHello("rangga"))
+}
